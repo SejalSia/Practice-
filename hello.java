659911c8d9c27fd356b78e086547407e5154f75b
SejalSia <157037969+SejalSia@users.noreply.github.com>
2025-08-25 12:48:38 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,21 @@
+class hello{
+    public static void main(String a[]){
+        System.out.println("Hello World");
+        System.out.println(3+8);
+        int num1 = 3;
+        int num2 = 4;
+        int result =num1 + num2;
+        System.out.println(num1+num2);
+        System.out.println(result);
+        //primitive(in built) data types
+        byte by = 127;
+        short sh = 446;
+        long l = 6783l;
+        float f = 6.2f;
+        double d = 7.3; //literals
+        char c = 'f';
+        boolean b = true;
+
+
+    }
+}
